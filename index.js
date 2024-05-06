da10bc896343b5845b36b7287eb8977b9ca10653
tieppnghien222 <dailytest1pun0suy@hotmail.com>
2024-05-06 23:08:12 +0700

    start

---
+++ index.js
@@ -0,0 +1,17 @@
+function levelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      level.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+  }
+  return result;
+}
